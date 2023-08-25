ec80b3b1aed9f4258374667f990fe8e38b56ec6d
Bektur <Bekturhanazarov98@gmail.com>
2023-08-25 18:45:44 +0600

    a

--- src/main/java/com/example/airbnb/service/HouseService.java
+++ src/main/java/com/example/airbnb/service/HouseService.java
@@ -1,7 +1,7 @@
-package com.example.airbnb.Service;
+package com.example.airbnb.service;
 
+import com.example.airbnb.dto.response.HouseResponse;
 import com.example.airbnb.dto.response.HouseResponseFindByID;
-import com.example.airbnb.dto.response.HouseResponseForGetAll;
 import com.example.airbnb.models.House;
 import com.example.airbnb.repositories.HouseRepository;
 import lombok.RequiredArgsConstructor;
@@ -65,20 +65,43 @@ public class HouseService {
     }
 
 
-    public List<HouseResponseForGetAll> findAll() {
-        List<HouseResponseForGetAll> list = new ArrayList<>();
-        for (House house : houseRepository.findAll()) {
-            HouseResponseForGetAll houseResponseForGetAll = new HouseResponseForGetAll();
-            houseResponseForGetAll.setTitle(house.getTitle());
-            houseResponseForGetAll.setRegion(house.getRegion());
-            houseResponseForGetAll.setPrice(String.valueOf(house.getPrice()));
-            houseResponseForGetAll.setMaxGuest(house.getMaxGuests());
-            houseResponseForGetAll.setRating(house.getRating());
-            houseResponseForGetAll.setImages(house.getPhotos());
-
+//    public List<HouseResponseForGetAll> findAll() {
+//        List<HouseResponseForGetAll> list = new ArrayList<>();
+//        for (House house : houseRepository.findAll()) {
+//            HouseResponseForGetAll houseResponseForGetAll = new HouseResponseForGetAll();
+//            houseResponseForGetAll.setTitle(house.getTitle());
+//            houseResponseForGetAll.setRegion(house.getRegion());
+//            houseResponseForGetAll.setPrice(String.valueOf(house.getPrice()));
+//            houseResponseForGetAll.setMaxGuest(house.getMaxGuests());
+//            houseResponseForGetAll.setRating(house.getRating());
+//            houseResponseForGetAll.setImages(house.getPhotos());
+//
+//        }
+//
+//        return list;
+//    }
+public List<HouseResponse> searchByTownOrTitleIgnoreCase(String searchTerm) {
+    List<HouseResponse> list1 = new ArrayList<>();
+    String cleanSearchTerm = cleanInput(searchTerm);
+    for (House house : searchHousesRepository.findAll()) {
+        if (house.getRegion().name().equalsIgnoreCase(cleanSearchTerm) ||
+                house.getTown().toLowerCase().contains(cleanSearchTerm) ||
+                house.getTitle().toLowerCase().contains(cleanSearchTerm)) {
+            HouseResponse houseResponse = new HouseResponse();
+            houseResponse.setId(house.getId());
+            houseResponse.setRegion(house.getRegion().name());
+            houseResponse.setTown(house.getTown());
+            houseResponse.setTitle(house.getTitle());
+            list1.add(houseResponse);
         }
-
-        return list;
     }
+
+    return list1;
+}
+    private String cleanInput(String input) {
+        return input.trim().replaceAll("[-,_]", "").replaceAll("\\s+", "").toLowerCase();
+    }
+}
+
 
 }
